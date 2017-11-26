9c020cd33dee4e2339c03d329b395d21d7c7bc0e
Corrado Parisi <corradoparisi@hotmail.com>
2017-11-26 17:49:53 +0100

    Add objectmapper, objectwriter and add annotations to starter class

--- src/main/java/ch/fhnw/tutorial/springangularapp/SpringAngularAppApplication.java
+++ src/main/java/ch/fhnw/tutorial/springangularapp/SpringAngularAppApplication.java
@@ -1,9 +1,13 @@
 package ch.fhnw.tutorial.springangularapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class SpringAngularAppApplication {
 
 	public static void main(String[] args) {
