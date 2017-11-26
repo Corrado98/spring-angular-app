fc9c45df8d0ac6a40d32ae23baf0cf0e744c690c
Corrado Parisi <corradoparisi@hotmail.com>
2017-11-26 19:03:40 +0100

    Add POST, PUT, DELETE structure and create webapp

--- src/main/java/ch/fhnw/tutorial/springangularapp/rest/ReservationResource.java
+++ src/main/java/ch/fhnw/tutorial/springangularapp/rest/ReservationResource.java
@@ -1,14 +1,12 @@
 package ch.fhnw.tutorial.springangularapp.rest;
 
+import ch.fhnw.tutorial.springangularapp.model.request.ReservationRequest;
 import ch.fhnw.tutorial.springangularapp.model.response.ReservationResponse;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
@@ -25,5 +23,28 @@ public class ReservationResource {
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
             LocalDate checkout) {
         return new ResponseEntity<>(new ReservationResponse(), HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<ReservationResponse> createReservation(
+            @RequestBody ReservationRequest reservationRequest) {
+
+        return new ResponseEntity<>(new ReservationResponse(), HttpStatus.CREATED);
+
+    }
+
+    @RequestMapping(path = "", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<ReservationResponse> updateReservation(
+            @RequestBody ReservationRequest reservationRequest) {
+
+        return new ResponseEntity<>(new ReservationResponse(), HttpStatus.OK);
+
+    }
+
+    @RequestMapping(path = "/{reservationId}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> deleteReservation(
+            @PathVariable long reservationId) {
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
